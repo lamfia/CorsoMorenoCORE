20c9a0d9d7058fd027a6c9775de40143aa755f2e
lamfia <gabrielguerrait@gmail.com>
2020-01-31 15:35:22 +0100

    Master

---
+++ CorsoCoreGabriel/Controllers/CoursesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorsoCoreGabriel.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //sdasjdo
+    }
+}
