7cebf8a4f650c83aaa5a8cc39671074783d8bbae
lamfia <gabrielguerrait@gmail.com>
2020-02-07 10:25:33 +0100

    Crezione cartella Models e il servizio di Course

---
+++ CorsoCoreGabriel/Models/Services/Application/CourseService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using CorsoCoreGabriel.Models.ViewModels;
+
+namespace CorsoCoreGabriel.Models.Services.Application
+{
+
+    public class CourseService
+    {
+        public List<CourseViewModel> GetCourses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ CorsoCoreGabriel/Models/ViewModels/CourseViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CorsoCoreGabriel.Models.ViewModels
+{
+
+public class CourseViewModel
+{
+
+
+}
+
+}
